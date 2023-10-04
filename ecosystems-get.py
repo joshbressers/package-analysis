b1ca4e541c2a63683cb5af0a030881d40a2db6f9
Josh Bressers <josh@bress.net>
2023-10-04 09:06:36 -0500

    Update the script
    Signed-off-by: Josh Bressers <josh@bress.net>

--- ecosystems-get.py
+++ ecosystems-get.py
@@ -8,20 +8,39 @@ from tqdm import tqdm
 import threading, queue
 import time
 import logging
+import sys
+
+class Registry:
+
+    def __init__(self, registry_name):
+        self.name = registry_name
+        self.url = "https://packages.ecosyste.ms/api/v1/registries/%s" % (registry_name)
+
+        # Setup http session
+        self.session = requests.Session()
+        retries = Retry(total=25,
+                        backoff_factor=0.1,
+                        status_forcelist=[ 500, 502, 503, 504 ])
+        self.session.mount('https://', HTTPAdapter(max_retries=retries))
+
+        self.packages_count = None
+
+    def get_packages_count(self):
+        if self.packages_count is None:
+            resp = self.session.get(url=self.url, timeout=30)
+            self.packages_count = int(resp.json()["packages_count"])
+        return self.packages_count
+
+    def get_num_pages(self):
+        # The pages start at 1, so add one
+        return int((self.get_packages_count() / 100) + 1)
+
+
 
 
 # Setup URL things
-npm_url = "https://packages.ecosyste.ms/api/v1/registries/npmjs.org"
-package_url = "https://packages.ecosyste.ms/api/v1/registries/npmjs.org/packages?page=%d&per_page=100&sort=name"
-version_url = "https://packages.ecosyste.ms/api/v1/registries/npmjs.org/packages/%s/versions?page=1&per_page=100"
-
-s = requests.Session()
-
-retries = Retry(total=500,
-                backoff_factor=0.1,
-                status_forcelist=[ 500, 502, 503, 504 ])
-
-s.mount('https://', HTTPAdapter(max_retries=retries))
+package_url = "https://packages.ecosyste.ms/api/v1/registries/%s/packages?page=%d&per_page=100&sort=name"
+version_url = "https://packages.ecosyste.ms/api/v1/registries/%s/packages/%s/versions?page=1&per_page=100"
 
 
 page_q = queue.Queue()
@@ -29,25 +48,35 @@ package_output_q = queue.Queue(maxsize=3000)
 version_output_q = queue.Queue(maxsize=3000)
 version_q = queue.Queue(maxsize=3000)
 
-package_es = Documents('npm-package-ecosystems', mapping='', delete=True)
-versions_es = Documents('npm-versions-ecosystems', mapping='', delete=True)
+package_es = Documents('ecosystems-package', mapping='', delete=False)
+versions_es = Documents('ecosystems-versions', mapping='', delete=False)
 
-def version_worker():
+def version_worker(repo):
+    # Setup http session
+    s = requests.Session()
+    retries = Retry(total=25,
+                    backoff_factor=0.1,
+                    status_forcelist=[ 500, 502, 503, 504 ])
+    s.mount('https://', HTTPAdapter(max_retries=retries))
 
     while True:
         package = version_q.get()
 
-        new_url = version_url % (package)
+        new_url = version_url % (repo, package)
         my_data = []
         try:
             response = s.get(url=new_url)
             version_json = response.json()
-
-            my_data.extend(version_json)
+            for i in version_json:
+                i["repo"] = repo
+                my_data.append(i)
 
             while response.links.get('next'):
                 response = s.get(response.links['next']['url'])
-                my_data.extend(response.json())
+                version_json = response.json()
+                for i in version_json:
+                    i["repo"] = repo
+                    my_data.append(i)
 
         except:
             # If something bad happens, just put the page back in the queue
@@ -62,11 +91,18 @@ def version_worker():
         version_q.task_done()
 
 
-def rest_worker():
+def package_worker(repo):
+    # Setup http session
+    s = requests.Session()
+#    retries = Retry(total=500,
+#                    backoff_factor=0.1,
+#                    status_forcelist=[ 500, 502, 503, 504 ])
+#    s.mount('https://', HTTPAdapter(max_retries=retries))
+
     while True:
         page = page_q.get()
 
-        new_url = package_url % (page)
+        new_url = package_url % (repo, page)
         try:
             resp = s.get(url=new_url)
             package_json = resp.json()
@@ -76,26 +112,14 @@ def rest_worker():
 
                 version_q.put(p["name"])
 
-                my_data = {}
-                my_data["name"] = p["name"]
-                my_data["ecosystem"] = p["ecosystem"]
-                my_data["normalized_licenses"] = p["normalized_licenses"]
-                my_data["versions_count"] = p["versions_count"]
-                my_data["first_release_published_at"] = p["first_release_published_at"]
-                my_data["latest_release_published_at"] = p["latest_release_published_at"]
-                my_data["dependent_packages_count"] = p["dependent_packages_count"]
-                my_data["rankings"] = p["rankings"]
-                my_data["advisories"] = p["advisories"]
-                my_data["downloads"] = p["downloads"]
-                my_data["downloads_period"] = p["downloads_period"]
-                my_data["maintainers"] = p["maintainers"]
-                my_data["num_maintainers"] = len(p["maintainers"])
+                p["repo"] = repo
+                p["num_maintainers"] = len(p["maintainers"])
 
-                package_output_q.put(my_data)
+                package_output_q.put(p)
 
         except:
             # If something bad happens, just put the page back in the queue
-            logging.warn("Version: Something bad happened")
+            logging.warn("Package: Something bad happened")
             page_q.put(page)
 
         page_q.task_done()
@@ -103,35 +127,38 @@ def rest_worker():
 def package_output_worker():
     while True:
         result = package_output_q.get()
-        doc_id = result["name"]
+        doc_id = "%s-%s" % (result["repo"], result["name"])
         package_es.add(result, doc_id)
         package_output_q.task_done()
 
 def version_output_worker():
     while True:
         result = version_output_q.get()
-        doc_id = "%s-%s" % (result["name"], result["number"])
+        doc_id = "%s-%s-%s" % (result["repo"], result["name"], result["number"])
         versions_es.add(result, doc_id)
         version_output_q.task_done()
 
-def get_packages_number():
-    resp = s.get(url=npm_url, timeout=30)
-    return resp.json()["packages_count"]
-
 def main():
 
-    num_packages = get_packages_number()
-    # The pages start at 1, so add one
-    total_pages = int((num_packages / 100) + 1)
+    if len(sys.argv) != 2:
+        print("Usage: %s <repo>" % (sys.argv[0]))
+        sys.exit(1)
+
+    repo_name = sys.argv[1]
+
+    my_repo = Registry(repo_name)
+    total_pages = my_repo.get_num_pages()
+
     for page in range(1, total_pages + 1):
         page_q.put(page)
 
     threading.Thread(target=package_output_worker, daemon=True).start()
     threading.Thread(target=version_output_worker, daemon=True).start()
+
     for i in range(15):
-        threading.Thread(target=version_worker, daemon=True).start()
-    for i in range(10):
-        threading.Thread(target=rest_worker, daemon=True).start()
+        threading.Thread(target=version_worker, args=(repo_name,), daemon=True).start()
+    for i in range(2):
+        threading.Thread(target=package_worker, args=(repo_name,), daemon=True).start()
 
     progress = tqdm(total=total_pages)
 
