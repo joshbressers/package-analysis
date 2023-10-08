59edbb5fe2390baf5e1a4bec85043c2d614a7493
Josh Bressers <josh@bress.net>
2023-10-07 20:13:39 -0500

    Add a script to load the ecosystems I like
    Signed-off-by: Josh Bressers <josh@bress.net>

---
+++ load.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+./ecosystems-get.py crates.io
+./ecosystems-get.py rubygems.org
+./ecosystems-get.py repo1.maven.org
+./ecosystems-get.py pypi.org
+./ecosystems-get.py nuget.org
+./ecosystems-get.py proxy.golang.org
+./ecosystems-get.py npmjs.org
+
